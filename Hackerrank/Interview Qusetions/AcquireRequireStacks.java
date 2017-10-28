b25d941ee08eb85d003986c92681db1559fb96dd
Arjun  Dass <ad.sam007@gmail.com>
2017-10-27 23:04:58 -0400

    Working program

--- Hackerrank/Interview Qusetions/AcquireRequireStacks.java
+++ Hackerrank/Interview Qusetions/AcquireRequireStacks.java
@@ -2,14 +2,12 @@ import java.util.*;
 class AcquireRequireStacks{
 
     public static void lockCheck(ArrayList<String> locks){
-
-        System.out.println(locks.toString());
+        
         Stack<Integer> locks_stack = new Stack<Integer>();
 
         for(int i=0; i<locks.size(); i++){
             
             String lock_type = locks.get(i).split(" ")[0];
-            System.out.println(lock_type);
             int lock_id = Integer.parseInt(locks.get(i).split(" ")[1]);
 
             if(lock_type.equals("ACQUIRE")){
@@ -20,12 +18,11 @@ class AcquireRequireStacks{
                 }
             }else if(lock_type.equals("RELEASE")){
 
-                System.out.println(locks_stack.peek().toString());
-                // if(locks_stack.peek() == lock_id){
-                //     locks_stack.pop();
-                // }else{
-                //     System.out.println("Can't release the current lock. @ " + i);
-                // }
+                if(locks_stack.peek() == lock_id){
+                    locks_stack.pop();
+                }else{
+                    System.out.println("Can't release the current lock. @ " + i);
+                }
             }
         }
 
@@ -39,14 +36,10 @@ class AcquireRequireStacks{
     public static void main(String args[]){
 
         ArrayList<String> locks = new ArrayList<String>();
-        Scanner scan = new Scanner(System.in);
-        while(scan.hasNext()){
-
-            if(scan.nextLine().equals("123")){
-                break;
-            }
-            locks.add(scan.nextLine());
-        }
+        
+        locks.add("ACQUIRE 123");
+        locks.add("ACQUIRE 321");
+        locks.add("RELEASE 123");
 
         lockCheck(locks);
     }
