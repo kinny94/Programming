c9a5388f805b71eb820f4476705d05fd673df9cd
kinny94 <ad.sam007@gmail.com>
2017-12-09 22:50:08 -0500

    Working on hard problem

--- Hackerrank/Interview Qusetions/PixuredHard.java
+++ Hackerrank/Interview Qusetions/PixuredHard.java
@@ -1,5 +1,51 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class PixuredHard{
+    
+
+    public int solution(int[] a, int[] b){
+        
+        int number_of_elements = a.length;
+        double c[] = new double[number_of_elements];
+        ArrayList<ArrayList> pairs = new ArrayList<>();
+    
+        for(int i=0; i<number_of_elements; i++){
+            c[i] =  ( b[i] / (double) 1000000);
+        }
+
+        for(int i=0; i<number_of_elements; i++){
+            System.out.print(c[i] + " | ");
+        }
+
+        for(int i=0; i<number_of_elements; i++){
+            
+        }
+
+        return 0;
+    }
+
+
     public static void main(String[] args) {
         
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How any elements?");
+        int number_of_elements  = scan.nextInt();
+
+        int a[] = new int[number_of_elements];
+        int b[] = new int[number_of_elements];
+
+        System.out.println("Enter elements for Array 1");
+        for( int i=0; i<number_of_elements; i++ ){
+            a[i] = scan.nextInt();
+        }
+
+        System.out.println("Enter elements for Array 2");
+        for( int i=0; i<number_of_elements; i++ ){
+            b[i] = scan.nextInt();
+        }
+
+        PixuredHard obj = new PixuredHard();
+        obj.solution(a, b);
     }
 }
