ab48616a976e6a2698d9d426992454840cc0fb38
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-12-10 21:47:36 -0500

    1st working program

--- Hackerrank/Interview Qusetions/PixuredHard.java
+++ Hackerrank/Interview Qusetions/PixuredHard.java
@@ -1,28 +1,50 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 class PixuredHard{
     
+    private static int count;
+
+    public static void createPairs(double[] c, int[] used, int startIndex, int usedCount){
+            
+		if(usedCount == used.length){
+
+            if(count >= 1000000){
+                return;
+            }
+
+            if(used[0] >= 0 && used[1] > used[0] && used[1] < c.length){
+                if( c[used[0]]*c[used[1]]  >= c[used[0]] + c[used[1]] ){
+                    count++;
+                }
+            }
+
+		}else{
+			for(int i = startIndex; i < c.length; i++){
+				used[usedCount] = i;
+				createPairs(c, used, i+1, usedCount+1);
+				used[usedCount] = -1;
+			}
+        }
+	}
 
     public int solution(int[] a, int[] b){
-        
+
         int number_of_elements = a.length;
         double c[] = new double[number_of_elements];
-        ArrayList<ArrayList> pairs = new ArrayList<>();
-    
+        int subGroupLength = 2;
+        int used[] = new int[subGroupLength];
+        Arrays.fill(used, -1);
+        
         for(int i=0; i<number_of_elements; i++){
-            c[i] =  ( b[i] / (double) 1000000);
+            c[i] =  a[i] + ( b[i] / (double) 1000000);
         }
 
-        for(int i=0; i<number_of_elements; i++){
-            System.out.print(c[i] + " | ");
-        }
-
-        for(int i=0; i<number_of_elements; i++){
-            
-        }
-
-        return 0;
+        createPairs(c, used, 0, 0);
+        return count;
     }
 
 
@@ -46,6 +68,6 @@ class PixuredHard{
         }
 
         PixuredHard obj = new PixuredHard();
-        obj.solution(a, b);
+        System.out.println(obj.solution(a, b));
     }
 }
