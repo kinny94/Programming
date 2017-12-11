e3ccdf93500d7194cbd636ce76ce9553593a0e2c
kinny94 <ad.sam007@gmail.com>
2017-12-11 13:03:37 -0500

    Added comments

--- Interview Qusetions/Pixured.java
+++ Interview Qusetions/Pixured.java
@@ -1,3 +1,7 @@
+/* The following program(function) returns the length of the linkedlist. I have implemented a simple linkedlist identical
+to the one in the program so that I can implement my function.
+*/
+
 class IntList{
     public int value;
     public IntList next;
@@ -9,16 +13,13 @@ class IntList{
 }
 
 class Pixured{
+
+    // declaring the head of the list
     IntList head;
 
-    public void push(int data){
-        IntList newNode = new IntList(data);
-        newNode.next = head;
-        head = newNode;
-    }
-    
-    public int getCount(){
-
+    // The function that returns the length of the linkedList
+    public int solution(){
+        
         IntList temp = head;
         int count = 0;
         while(temp != null){
@@ -29,6 +30,14 @@ class Pixured{
         return count;
     }
 
+    // A push fucntion to add data to the linkedList
+    public void push(int data){
+        IntList newNode = new IntList(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // A function to print the list
     public void printlList(){
         while(head != null){
             System.out.print(head.value + " -> ");
@@ -46,7 +55,9 @@ class Pixured{
         list.push(5);
         list.push(6);
 
-        System.out.println("The length of the list is " + list.getCount());
-        list.printlList();
+        System.out.println("The length of the list is " + list.solution());
+        
+        // Comment out the following line to print the list
+        //list.printlList();
     }
 }
--- Interview Qusetions/PixuredHard.java
+++ Interview Qusetions/PixuredHard.java
@@ -1,30 +1,43 @@
-import java.util.ArrayList;
+/* The following program returns the number of mulitplicative pairs if the pair satiasfy the equation 
+0 ≤ P < Q < N and C[P] * C[Q] ≥ C[P] + C[Q]. If the number of multiplicative pairs is greater than 1,000,000,000, 
+the function returns 1,000,000,000.
+
+I have used recurrsion to compute the indices pair so that i can compute the product and sum.
+
+The worst case time complexity of the program is O(n)
+The worst case space complexity of the program is a little greater than O(1),  because I have used an integer array with at most 2 elements 
+in it. 
+
+I have also added a timer to calculate the running time of my function, I have commented out the part that calculates the running time.
+Inorder to see the running time just remove the comments from the timers in the main method.
+
+*/
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
 import java.util.Timer;
 import java.util.concurrent.TimeUnit;
 import java.time.Instant;
 import java.time.Duration;
-import java.lang.Thread;
 
 class PixuredHard{
     
-    private static int count;
+    private static int count;   // To keep the count of indices pair which satisfy the equation
 
     public static void createPairs(double[] c, int[] used, int startIndex, int usedCount){
-            
+         
+        //  If the usedCount is equal to length of user array (2). Considering only two elements of the array.
 		if(usedCount == used.length){
 
-            if(count >= 1000000){
+            // If the count is more than 1000000000
+            if(count >= 1000000000){
                 return;
             }
 
+            // checking the condition 0 ≤ P < Q < N and C[P] * C[Q] ≥ C[P] + C[Q]. 
             if(used[0] >= 0 && used[1] > used[0] && used[1] < c.length){
                 if( c[used[0]]*c[used[1]]  >= c[used[0]] + c[used[1]] ){
                     count++;
-                    //System.out.println(Arrays.toString(used));
+                    //System.out.println(Arrays.toString(used)); // To print out the pairs
                 }
             }
 
@@ -35,21 +48,31 @@ class PixuredHard{
 				used[usedCount] = -1;
 			}
         }
-	}
+    }
 
     public int solution(int[] a, int[] b){
 
 
         int number_of_elements = a.length;
+
+        // Array to store the sum of two arrays
         double c[] = new double[number_of_elements];
-        int pairLength = 2;
-        int used[] = new int[pairLength];
-        Arrays.fill(used, -1);
         
+        // limiting No of elements to make group of
+        int pairLength = 2;
+       
+        // An empty array of size to store two pair indices
+        int used[] = new int[pairLength];       
+        
+        // Filling the array with -1 at the first place.
+        Arrays.fill(used, -1);                  
+        
+        // Calculating the C array
         for(int i=0; i<number_of_elements; i++){
             c[i] =  a[i] + ( b[i] / (double) 1000000);
         }
 
+        // Calling the reccursive function
         createPairs(c, used, 0, 0);
 
         return count;
@@ -76,13 +99,13 @@ class PixuredHard{
         }
 
         PixuredHard obj = new PixuredHard();
-
-        //long startTime = System.nanoTime();
         
         Instant start = Instant.now();
+        
         System.out.println(obj.solution(a, b));
+        
         Instant end = Instant.now();
-        System.out.println(Duration.between(start, end));
-        //System.out.println(totalTime);
+        System.out.println("Time taken : " + Duration.between(start, end));
+        
     }
 }
