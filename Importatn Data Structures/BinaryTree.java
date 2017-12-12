7e7f6c2b3087990c8f835af2a03f35644f78e3ce
kinny94 <ad.sam007@gmail.com>
2017-12-12 17:13:31 -0500

    Delete node functionality

--- Importatn Data Structures/BinaryTree.java
+++ Importatn Data Structures/BinaryTree.java
@@ -1,3 +1,5 @@
+import javax.swing.text.html.FormView;
+
 import com.sun.javafx.stage.FocusUngrabEvent;
 
 class Node{
@@ -79,6 +81,118 @@ class BinaryTree{
         }
     }
 
+    public Node findNode(int key){
+
+        Node focusNode = root;
+
+        while(focusNode.key != key){
+
+            if(key < focusNode.key){
+                focusNode = focusNode.leftChild;
+            }else{
+                focusNode = focusNode.rightChild;
+            }
+
+            if(focusNode == null){
+                return null;
+            }
+        }
+
+        return focusNode;
+    } 
+
+    public boolean remove(int key){
+        
+        Node focusNode = root;
+        Node parent = root;
+
+        boolean isItALeftChild = true;
+
+        while(focusNode.key != key){
+            parent = focusNode;
+
+            if(key < focusNode.key){
+                isItALeftChild = true;
+                focusNode = focusNode.leftChild;
+            }else{
+                isItALeftChild = false;
+                focusNode = focusNode.rightChild;
+            }
+
+            if(focusNode == null){
+                return false;
+            }
+        }
+
+        if(focusNode.leftChild == null && focusNode.rightChild == null){
+            
+            if(focusNode == root){
+                root = null;
+            }else if(isItALeftChild){
+                parent.leftChild = null;
+            }else{
+                parent.rightChild = null;
+            }
+
+        }
+
+        else if(focusNode.rightChild == null){
+            if(focusNode == root){
+                root = focusNode.leftChild;
+            }else if(isItALeftChild){
+                parent.leftChild = focusNode.leftChild;
+            }else{
+                parent.rightChild = focusNode.leftChild;
+            }
+        }
+
+        else if(focusNode.leftChild == null){
+            if(focusNode == root){
+                root = focusNode.rightChild;
+            }else if(isItALeftChild){
+                parent.leftChild = focusNode.rightChild;
+            }else{
+                parent.rightChild = focusNode.leftChild;
+            }
+        }
+
+        else{
+            Node replacementNode = getReplacementNode(focusNode);
+
+            if(focusNode == root){
+                root = replacementNode;
+            }else if(isItALeftChild){
+                parent.leftChild = replacementNode;
+            }else{
+                parent.rightChild = replacementNode;
+            }
+
+            replacementNode.leftChild = focusNode.leftChild;
+        }
+
+        return true;
+    }
+
+    public Node getReplacemeNode(Node replacedNode){
+        Node replacementParent = replacedNode;
+        Node replacement = replacedNode;
+
+        Node focusNode = replacedNode.rightChild;
+
+        while(focusNode != null){
+            replacementParent = replacement;
+            replacement = focusNode;
+            focusNode = focusNode.leftChild;
+        }
+
+        if(replacement != replacedNode.rightChild){
+            replacementParent.leftChild = replacement.rightChild;
+            replacement.rightChild = replacedNode.rightChild;
+        }
+
+        return replacement;
+    }
+
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
         tree.addNode(50, "Boss");
@@ -94,6 +208,9 @@ class BinaryTree{
         System.out.println();
 
         tree.postOrderTraversal(tree.root);
+    
+        System.out.println("Search for 25");
+        System.out.println(tree.findNode(25));
     }
 }
 
