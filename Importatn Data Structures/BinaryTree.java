cf834fd5d45ab6db4f42405081048f1fc4a408ec
kinny94 <ad.sam007@gmail.com>
2017-12-12 11:43:49 -0500

    Implementing tree

---
+++ Importatn Data Structures/BinaryTree.java
@@ -0,0 +1,60 @@
+class Node{
+
+    int key;
+    String name;
+
+    Node rightChild;
+    Node leftChild;
+
+    Node(int key, String name){
+        this.key = key;
+        this.name = name;
+    }
+
+    public String toString(){
+        return name + " has  a key " + key; 
+    }
+}
+
+class BinaryTree{
+
+    Node root;
+
+    public void addNode(int key, String name){
+        
+        Node newNode = new Node(key, name);
+
+        if(root == null){
+            root = newNode;
+        }else{
+            
+            Node focusNode = root;
+            Node parent;
+
+            while(true){
+                
+                parent = focusNode;
+
+                if(key < focusNode.key){
+                    focusNode = focusNode.leftChild;
+
+                    if(focusNode == null){
+                        parent.leftChild = newNode;
+                        return;
+                    }
+                }else{
+                    focusNode = focusNode.rightChild;
+                    if(focusNode == null){
+                        parent.rightChild = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
+
