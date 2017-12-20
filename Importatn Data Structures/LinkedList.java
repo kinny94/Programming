e7e44b1749da06dfef8d12d523e1d284879be482
kinny94 <ad.sam007@gmail.com>
2017-12-20 12:21:33 -0500

    Added LinkedList Program

---
+++ Importatn Data Structures/LinkedList.java
@@ -0,0 +1,102 @@
+import com.sun.webkit.ThemeClient;
+
+class Link{
+    public int current;
+    public Link next;
+
+    Link(int current){
+        this.current = current;
+    }
+
+    public void display(){
+        System.out.println("Current : " + current);
+    }
+
+    public String toString(){
+        return Integer.toString(current);
+    }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insertFirst(5);
+        linkedList.insertFirst(10);
+        linkedList.insertFirst(15);
+        linkedList.insertFirst(20);
+        linkedList.insertFirst(25);
+
+        linkedList.display();
+
+    }ist linkedList = new LinkedList();
+        linkedList.insertFirst(5);
+        linkedList.insertFirst(10);
+        linkedList.insertFirst(15);
+        linkedList.insertFirst(20);
+        linkedList.insertFirst(25);
+
+    }ist linkedList = new LinkedList();
+        linkedList.insertFirst(5);
+        linkedList.insertFirst(5);
+        linkedList.insertFirst(5);
+        linkedList.insertFirst(5);
+    // check if the link is empty or not
+        linkedList.insertFirst(5);
+
+    }ist linkedList = new LinkedList();
+        linkedList.insertFirst(5);
+    }ist linkedList = new LinkedList();
+    }ist linkedList = new LinkedList()
+;    }
+    // check if the link is empty or not
+}
+
+class LinkedList{
+    
+    public Link firstLink;
+
+    LinkedList(){
+    // check if the link is empty or not
+        firstLink = null;
+    }
+    // check if the link is empty or not
+    
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    // check if the link is empty or not
+    public boolean isEmpty(){
+        return(firstLink == null);
+    }
+}
