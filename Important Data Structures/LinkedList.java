d26f1dab8143a3512824a548f4ce032d5e6b60e9
kinny94 <ad.sam007@gmail.com>
2018-01-04 15:22:20 -0500

    LinkedList implementation

--- Important Data Structures/LinkedList.java
+++ Important Data Structures/LinkedList.java
@@ -1,43 +1,124 @@
-class Link{
-    public int current;
-    public Link next;
+import java.util.*;
 
-    Link(int current){
-        this.current = current;
+class Link{
+    
+    public LinkedList firstLink;
+
+    Link(){
+        firstLink = null;
+    }
+
+    public boolean isEmpty(){
+        return firstLink == null;
+    }
+
+    public void insert(String name){
+        LinkedList newLink = new LinkedList(name);
+        newLink.next = firstLink;
+        firstLink = newLink;
+    }
+
+    public LinkedList removeFirst(){
+        
+        LinkedList linkReference = firstLink;
+        if(!isEmpty()){
+            firstLink = firstLink.next;
+        }else{
+            System.out.println("Empty Linked List!!");
+        }
+
+        return linkReference;
     }
 
     public void display(){
-        System.out.println("Current : " + current);
+        LinkedList theLink = firstLink;
+        while(theLink != null){
+            theLink.display();
+            System.out.println("Next Link : " + theLink.next);
+            theLink = theLink.next;
+            System.out.println();
+        }
     }
 
-    public String toString(){
-        return Integer.toString(current);
+    public LinkedList find(String name){
+        LinkedList theLink = firstLink;
+
+        if(!isEmpty()){
+            while(theLink != null){
+                if(theLink.next == null){
+                    return null;
+                }else{
+                    theLink = theLink.next;
+                }
+            }
+        }else{
+            System.out.println("EmptyLinked List!");
+        }
+
+        return theLink;
     }
 
-    public static void main(String[] args) {
-        
+    public LinkedList remove(String name){
+
+        LinkedList currentLink = firstLink;
+        LinkedList previousLink = firstLink;
+    
+        while(currentLink.name != name){
+            if(currentLink.next == null){
+                return null;
+            }else{
+                previousLink = currentLink;
+                currentLink = currentLink.next; 
+            }
+        }
+
+        if(currentLink == firstLink){
+            firstLink = firstLink.next;
+        }else{
+            previousLink.next = currentLink.next;
+        }
+
+        return currentLink;
     }
 }
 
 class LinkedList{
-    
-    public Link firstLink;
 
-    LinkedList(){
-        firstLink = null;
+    public String name;
+    public LinkedList next;
+
+    public LinkedList(String name){
+        this.name = name;
+    }
+
+    public void display(){
+        System.out.println("Current name is " + name);
+    }
+
+    public String toString(){
+        return name;    
     }
     
-    public boolean isEmpty(){
-        return(firstLink == null);
-    }
-}mpty or not
-    // check if the link is empty or not
-    // check if the link is empty or not
-    // check if the link is empty or not
-    // check if the link is empty or not
-    // check if the link is empty or not
-    // check if the link is empty or not
-    public boolean isEmpty(){
-        return(firstLink == null);
+    public static void main(String[] args) {
+    Link newLinkedList = new Link();
+    newLinkedList.insert("Harry Potter");
+    newLinkedList.insert("Lord of the Rings");
+    newLinkedList.insert("Alchemist");
+    newLinkedList.insert("Harry Potter");
+    newLinkedList.insert("Da Vinci Code");
+    newLinkedList.insert("50 Shades of Grey");
+    
+    newLinkedList.display();
+    System.out.println("*****************************\n");
+    newLinkedList.removeFirst();
+    newLinkedList.display();
+    System.out.println("*****************************\n");
+    
+    System.out.println(newLinkedList.find("Harry Potter"));
+    
+    System.out.println("*****************************\n");
+    
+    newLinkedList.remove("Alchemist");
+    newLinkedList.display();
     }
 }
