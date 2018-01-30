862c205d61b80abd88177966523245791960401d
kinny94 <ad.sam007@gmail.com>
2018-01-30 01:33:12 -0500

    Added everything

---
+++ Interview Qusetions/IsABST.java
@@ -0,0 +1,44 @@
+class Node{
+    
+    int data;
+    Node left, right;
+
+    public Node( int item ){
+        data = item;
+        left = right = null;
+    }
+}
+
+public class IsABST{
+    Node root;
+
+    boolean isBST(){
+        return isBSTUtil( root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+
+    boolean isBSTUtil( Node node, int min, int max ){
+        if( node == null ){
+            return true;
+        }
+
+        if( node.data < min || node.data > max ){
+            return false;
+        }
+        
+        return (isBSTUtil(node.left, min, node.data - 1) && isBSTUtil(node.right, node.data + 1, max));
+    }
+    
+    public static void main(String[] args) {
+        IsABST tree = new IsABST();
+        tree.root = new Node(4);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(5);
+        tree.root.left.left = new Node(1);
+        tree.root.left.right = new Node(3);
+ 
+        if (tree.isBST())
+            System.out.println("IS BST");
+        else
+            System.out.println("Not a BST");
+    }
+}
