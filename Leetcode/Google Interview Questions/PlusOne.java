21bfd8d3e2dbd1eb7dd2b15ddd4c195ffa54515f
kinny94 <ad.sam007@gmail.com>
2018-02-25 18:00:01 -0500

    Implemented Plus one program

---
+++ Leetcode/Google Interview Questions/PlusOne.java
@@ -0,0 +1,25 @@
+class PlusOne{
+
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i=n-1; i>=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int [n+1];
+        newNumber[0] = 1;
+
+        return newNumber;
+    }
+
+    public static void main(String[] args) {
+        PlusOne obj = new PlusOne();
+        int a[] = {1, 2, 4, 6};
+        System.out.println(obj.plusOne(a));
+    }
+}
