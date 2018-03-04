ab000c796f991be0f587e17eaa96d32751ec2e8a
Arjun Dass <ad.sam007@gmail.com>
2018-03-03 22:37:12 -0500

    Deleting sa linkx

--- Leetcode/Data Structures/SinglyLinkedList.java
+++ Leetcode/Data Structures/SinglyLinkedList.java
@@ -1,3 +1,5 @@
+import apple.laf.JRSUIConstants.State;
+
 public class Node{
 
     Node link = null;
@@ -25,7 +27,7 @@ public class Node{
         return data;
     }
 
-    public void setLink( int data ){
+    public void setData( int data ){
         this.data = data;
     }
 }
@@ -68,19 +70,126 @@ class LinkedList{
         size++;
     }
 
+    // inserting at a particular index O(N)
     public void insertAtposition( int data, int index ){
         Node pointer = new Node( data, null );
         Node nextNode = start;
         index = index - 1;
 
         for( int i=1; i<size; i++){
-            if( i== index ){
+            if( i == index ){
                 Node temp2 = nextNode.getLink();
                 nextNode.setLink( pointer );
                 pointer.setLink( temp2 );
                 break;
             }
-            nextPointer = nextNode.getLink();
+            nextNode = nextNode.getLink();
         }
     }
+
+    public void replaceDataAtPosition( int data, int position ){
+        if( start == null ){
+            Sytem.out.println("Empty!!");
+            return;
+        }
+
+        Node pointer = start;
+        for( int i=0; i<size; i++ ){
+            if( i == position ){
+                pointer.setData( data );
+            }
+            nextNode = pointer.getLink();
+        }
+    }
+
+    public void deleteAtPosition( int position ){
+
+        if( start == null ){
+            System.out.println("Empty!!");
+            return;
+        }
+
+        // Deleting the last element
+        if( position == size ){
+            Node startPointer = start; 
+            Node endPointer = start;
+            while( startPointer != null ){
+                endPointer = startPointer;
+                startPointer = startPointer.getLink();            
+            }      
+            end = endPointer;
+            end.setLink( null );
+            size--;
+            return;
+        }
+
+        //Deleting anywhere in the list
+        Node pointer = start;
+        position = position - 1;
+        for( int i=1; i<size; i++ ){
+            if( i == position ){
+                Node temp = pointer.getLink();
+                temp = temp.getLink();
+                pointer.setLink( temp );
+                break;
+            }
+            pointer = pointer.getLink();
+        }
+        size--;
+    }
+
+    public void deleteNodeByData( int data ){
+        if( start == null ){
+            System.out.println("Empty@!!");
+            return;
+        }
+
+        if( start.getData() == data  && start.getLink() == null ){
+            start = null;
+            end = null;
+            size--;
+            return;
+        } 
+
+        if( start.getData() == data && start.getLink() != null ){
+            start = start.getLink();
+            size--;
+            return;
+        }
+
+        if( end.getData() == data ){
+            Node startPointer = start;
+            Node endPointer = start;
+
+            startPointer = startPointer.getLink();
+            while( startPointer.getLink() != null ){
+                endPointer = startPointer;
+                startPointer = startPointer.getLink();
+            }
+
+            end = endPointer;
+            end.setLink( null );
+            size--;
+            return;
+        }
+
+        Node startPointer = start;
+        Node previousPointer = startPointer;
+        startPointer = startPointer.getLink();
+        while( startPointer.getData() != data && startPointer.getLink() != null ){
+            previousPointer = startPointer;
+            startPointer = startPointer.getLink();
+        }
+
+        if( startPointer.getData() == data ){
+            Node temp = previousPointer.getLink();
+            previousPointer.setLink( temp );
+            size--;
+            return;
+        }
+
+        System.out.println(data + " not found!!");
+    }
+
+
 }
