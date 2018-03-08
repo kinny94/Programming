9ac49757ffe6bd185b45ad248a41abb6268ddf82
Arjun Dass <ad.sam007@gmail.com>
2018-03-08 01:35:09 -0500

    Adjancency List using LinkedList class

---
+++ Leetcode/Data Structures/GraphAdjacencyList.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class GraphAdjacencyList{
+    int vertex;
+    LinkedList<Integer> list[];
+
+    public GraphAdjacencyList( int vertex ){
+        this.vertex = vertex;
+        list = new LinkedList[ vertex ];
+        for( int i=0; i<vertex; i++ ){
+            list[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge( int source, int destination ){
+        list[source].addFirst( destination );
+        list[destination].addFirst( source );
+    }
+
+    public void printGraph(){
+        for (int i = 0; i <vertex ; i++) {
+            if(list[i].size()>0) {
+                System.out.print("Vertex " + i + " is connected to: ");
+                for (int j = 0; j < list[i].size(); j++) {
+                    System.out.print(list[i].get(j) + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+        graph.addEdge(0,1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.printGraph();
+    }
+}
