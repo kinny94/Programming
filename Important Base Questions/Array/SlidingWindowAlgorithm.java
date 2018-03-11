3ec517c31a7179c015550e81bb6261c03d467de9
Arjun Dass <ad.sam007@gmail.com>
2018-03-11 17:27:41 -0400

    started working on a problem

---
+++ Important Base Questions/Array/SlidingWindowAlgorithm.java
@@ -0,0 +1,19 @@
+/*
+    Given an array and integer k, write an algorithm to find the maximum element in each subarray of size k.
+
+    int [] nums = { 1, 2, 3, 2, 4, 1, 5, 6,1};
+    Output: 3, 3, 4, 4, 5, 6, 6
+    Subarrays –
+    [1, 2, 3], max = 3
+    [2, 3, 2], max = 3
+    [3, 2, 4], max = 4
+    [2, 4, 1], max = 4
+    [4, 1, 5], max = 5
+    [1, 5, 6], max = 6
+    [5, 6, 1], max = 6
+
+*/
+
+class SlidingWindowAlgorithm{
+
+}
