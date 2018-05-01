610fdd359a4145f70000f2fef58127df9bf7d51f
kinny94 <ad.sam007@gmail.com>
2018-05-01 11:00:17 -0400

    hyland questyions

---
+++ Leetcode/Algorithms/Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Fibonacci
+{
+    public static int fibRec(int num) {
+        if (num == 0) {
+            return 0;
+        } else if (num == 1 || num == 2) {
+            return 1;
+        } else {
+            return fibRec(num - 1) + fibRec(num - 2);
+        }
+    }
+      
+    public static void main (String args[])
+    {
+    int n = 5;
+    System.out.println(fibRec(n));
+    }
+}
--- Leetcode/Algorithms/Hyland1.java
+++ Leetcode/Algorithms/Hyland1.java
@@ -1,18 +1,18 @@
 // implement a sorting algorithm
-// difference between URL and UNC
-// Fibonaaci series
-// SDLC
+// difference between URL and UNC - done
+// Fibonaaci series - done
+// SDLC - Ddone
 // FIzzbuzz - done
 // Four tenets of Object oriented Programming - done
 // Difference between a thread and a Process
-// blackbox testing
-// difference betqween a stack and a queue
+// blackbox testing - // done
+// difference betqween a stack and a queue - done
 // Words counts and print the most occuring words - done
-// create a function to figure if a string is a palindrome - 
-// create a function that return the first index of a character in a string
-// create a function that return the sum of an integer array
-// Given a string, return a list of the most common characters in the string.
-//How would you test a black box program that took in a string and a character 
+// create a function to figure if a string is a palindrome - done
+// create a function that return the first index of a character in a string - done
+// create a function that return the sum of an integer array - done
+// Given a string, return a list of the most common characters in the string. - done
+//How would you test a black box program that took in a string and a character  - done
 // and returned the number of occurrences of that character?  
 
 import java.util.*;
