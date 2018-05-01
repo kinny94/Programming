eca255797ed0293486f5c567422b6fc1b8f3e8f6
kinny94 <ad.sam007@gmail.com>
2018-04-30 23:26:26 -0400

    Hyland questions

---
+++ Leetcode/Algorithms/Hyland1.java
@@ -0,0 +1,33 @@
+// implement a sorting algorithm
+// difference between URL and UNC
+// Fibonaaci series
+// SDLC
+// FIzzbuzz
+// Four tenets of Object oriented Programming
+// Difference between a thread and a Process
+// blackbox testing
+// difference betqween a stack and a queue
+// Words counts and print the most occuring words
+// create a function to figure if a string is a palindrome
+// create a function that return the first index of a character in a string
+// create a function that return the sum of an integer array
+// Given a string, return a list of the most common characters in the string.
+//How would you test a black box program that took in a string and a character 
+// and returned the number of occurrences of that character?  
+
+import java.util.*;
+
+class Hyland1{
+
+    public static int computeSum( int i, int j ){
+        return i + j;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner( System.in );
+        int i = scan.nextInt();
+        int j = scan.nextInt();
+        
+        int sum = computeSum( i, j );
+    }
+}
