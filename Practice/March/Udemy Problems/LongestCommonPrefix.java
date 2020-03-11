b1b06ddd6a0c3bf0ecf37974f33e2b1e187db231
kinny <ad.sam007@gmail.com>
2020-03-10 22:38:33 -0400

    LongestCommonPrefix and String Prefix

---
+++ Practice/March/Udemy Problems/LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String text1, String text2) {
+        int commonLength = Math.min(text1.length(), text2.length());
+        for (int i=0; i<commonLength; i++) {
+            if (text1.charAt(i) != text2.charAt(i)) {
+                return text1.substring(0, i);
+            }
+        }
+
+        return text1.substring(0, commonLength);
+    }   
+
+    public static void main(String[] args) {
+        LongestCommonPrefix lcp = new LongestCommonPrefix();
+        System.out.println(lcp.longestCommonPrefix("helloo", "hellook"));
+    }
+}
