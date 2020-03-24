3bd498c249d8164055c77d90a204b7e69d5bf1d8
Arjun Dass <ad.sam007@gmail.com>
2020-03-23 23:21:25 -0400

    Stream of characters

---
+++ Main/Everything/Leetcode/Google/StreamOfCharacters.java
@@ -0,0 +1,42 @@
+class TrieNode {
+    boolean isWord;
+    TrieNode[] next = new TrieNode[26];
+}
+
+class StreamOfCharacters {
+
+    TrieNode root = new TrieNode();
+    StringBuilder sb = new StringBuilder();
+
+    public StreamOfCharacters(String[] words) {
+        createTrie(words);
+    }
+
+    public boolean query(char letter) {
+        sb.append(letter);
+        TrieNode node = root;
+        for (int i = sb.length() - 1; i >= 0 && node != null; i--) {
+            char c = sb.charAt(i);
+            node = node.next[c - 'a'];
+            if (node != null && node.isWord) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void createTrie(String[] words) {
+        for (String s : words) {
+            TrieNode node = root;
+            int len = s.length();
+            for (int i = len - 1; i >= 0; i--) {
+                char c = s.charAt(i);
+                if (node.next[c - 'a'] == null) {
+                    node.next[c - 'a'] = new TrieNode();
+                }
+                node = node.next[c - 'a'];
+            }
+            node.isWord = true;
+        }
+    }
+}
