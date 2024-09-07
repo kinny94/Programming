c0e72a17e64ca6e4b6dad26ccc07100a06f87d1e
Arjun Dass <ad.sam007@gmail.com>
2024-09-06 21:03:17 -0700

    Generic Stack implementation

--- Data Structures/src/datastructures/stacks/Stacks.java
+++ Data Structures/src/datastructures/stacks/Stacks.java
@@ -1,52 +1,57 @@
 package datastructures.stacks;
 
-public class Stacks {
+public class Stacks<T> {
 
-    int[] a;
-    int top = 0;
+    private T[] a;
+    private int top;
 
+    // Constructor to initialize stack with a given size
+    @SuppressWarnings("unchecked")
     Stacks(int size) {
-       a = new int[size];
+        a = (T[]) new Object[size];  // Create a generic array
+        top = -1;  // Initialize to -1 to indicate empty stack
     }
 
-    public void push(int x) {
-        // check if the stack if full
-        if (top == a.length) {
+    // Push method to add an element to the stack
+    public void push(T x) {
+        if (isFull()) {
             System.out.println("Stack is full!!");
-        }
-        a[top] = x;
-        if (top == a.length - 1) {
             return;
         }
-        top++;
+        a[++top] = x;  // Increment top and assign value
     }
 
-    public Integer pop() {
+    // Pop method to remove and return the top element
+    public T pop() {
         if (isEmpty()) {
             System.out.println("Stack is empty!!");
+            return null;
         }
-        int temp = a[top];
-        a[top--] = 0;
-        return temp;
+        return a[top--];  // Return value and decrement top
     }
 
-    public Integer peek() {
-        if (isFull()) {
+    // Peek method to view the top element without removing it
+    public T peek() {
+        if (isEmpty()) {
             System.out.println("Stack is empty!!");
+            return null;
         }
-        return a[top - 1];
+        return a[top];  // Return current top value
     }
 
+    // Check if the stack is empty
     public boolean isEmpty() {
-        return top == 0;
+        return top == -1;  // Stack is empty if top is -1
     }
 
+    // Check if the stack is full
     public boolean isFull() {
-        return top == a.length - 1;
+        return top == a.length - 1;  // Stack is full if top is at the last index
     }
 
+    // Print all elements in the stack
     public void print() {
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i <= top; i++) {
             System.out.print(a[i] + " ");
         }
         System.out.println();
@@ -54,22 +59,30 @@ public class Stacks {
         System.out.println();
     }
 
+    // Main method to test the generic stack
     public static void main(String[] args) {
-        Stacks s = new Stacks(5);
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.print();
-        System.out.println(s.peek());
-        s.push(4);
-        s.push(5);
-        s.print();
-        System.out.println(s.pop());
-        System.out.println(s.pop());
-        s.print();
-        System.out.println(s.pop());
-        System.out.println(s.pop());
-        s.print();
-        System.out.println(s.pop());
+        Stacks<Integer> intStack = new Stacks<>(5);
+        intStack.push(1);
+        intStack.push(2);
+        intStack.push(3);
+        intStack.print();
+        System.out.println("Peek: " + intStack.peek());
+        intStack.push(4);
+        intStack.push(5);
+        intStack.print();
+        System.out.println("Pop: " + intStack.pop());
+        System.out.println("Pop: " + intStack.pop());
+        intStack.print();
+
+        // Testing with String type
+        Stacks<String> stringStack = new Stacks<>(3);
+        stringStack.push("Hello");
+        stringStack.push("World");
+        stringStack.print();
+        System.out.println("Peek: " + stringStack.peek());
+        stringStack.push("Java");
+        stringStack.print();
+        System.out.println("Pop: " + stringStack.pop());
+        stringStack.print();
     }
 }
