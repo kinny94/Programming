a2e2aad5f501f0bb60af71f5273c50f7e9c2546c
Arjun Dass <ad.sam007@gmail.com>
2024-11-20 18:34:16 -0800

    implement maze problem

---
+++ Data Structures/src/algorithms/Backtracking/MazeProblem.java
@@ -0,0 +1,91 @@
+package algorithms.Backtracking;
+
+public class MazeProblem {
+
+    private int[][] maze;
+    private int[][] solution;
+    private int mazeSize;
+
+    MazeProblem(int[][] maze) {
+        this.maze = maze;
+        this.mazeSize = maze.length;
+        this.solution = new int[mazeSize][mazeSize];
+    }
+
+    public void solve() {
+        if (findSolution(0, 0)) {
+            showSolution();
+        } else {
+            System.out.println("There is no solution");
+        }
+    }
+
+    private boolean findSolution(int rowIndex, int colIndex) {
+        // base case - If we have reached the end
+        if (isFinished(rowIndex, colIndex)) {
+            return true;
+        }
+
+        if (isValid(rowIndex, colIndex)) {
+            solution[rowIndex][colIndex] = 1;
+
+            // Going right - go forward in the horizontal direction
+            if (findSolution(rowIndex, colIndex + 1)) {
+                return true;
+            }
+
+            // Going down - go downward in the vertical direction
+            if (findSolution(rowIndex + 1, colIndex)) {
+                return true;
+            }
+
+            // backtracking
+            solution[rowIndex][colIndex] = 0;
+        }
+        return false;
+    }
+
+    private boolean isFinished(int rowIndex, int colIndex) {
+        // reached end
+        if (rowIndex == mazeSize - 1 && colIndex == mazeSize - 1) {
+             solution[rowIndex][colIndex] = 1;
+             return true;
+        }
+        return false;
+    }
+
+    private boolean isValid(int rowIndex, int colIndex) {
+        if (rowIndex < 0 || rowIndex >= mazeSize) {
+            return false;
+        }
+        if (colIndex < 0 || colIndex >= mazeSize) {
+            return false;
+        }
+
+        return maze[rowIndex][colIndex] != 0;
+    }
+
+    private void showSolution() {
+        for (int i = 0; i < mazeSize; i++) {
+            for (int j = 0; j < mazeSize; j++) {
+                if (solution[i][j] == 1) {
+                    System.out.print(" S ");
+                } else {
+                    System.out.print(" - ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] maze = {
+                {1, 1, 1, 1},
+                {1, 0, 1, 0},
+                {0, 0, 1, 1},
+                {1, 0, 1, 1}
+        };
+        MazeProblem mazeProblem = new MazeProblem(maze);
+        mazeProblem.solve();
+    }
+}
