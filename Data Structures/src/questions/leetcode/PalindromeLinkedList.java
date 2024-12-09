afee99035518c773cd3ef13c0838465871bebc78
Arjun Dass <ad.sam007@gmail.com>
2024-12-09 01:39:39 -0800

    Palindrome linked list

---
+++ Data Structures/src/questions/leetcode/PalindromeLinkedList.java
@@ -0,0 +1,55 @@
+package questions.leetcode;
+
+public class PalindromeLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        // compare
+        ListNode first = head;
+        ListNode second = prev;
+        while (second != null) {
+            if (first.val != second.val) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+}
