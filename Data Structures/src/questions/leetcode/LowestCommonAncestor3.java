263a52f483a73e8f0854ed208601fd48a5c99b9e
Arjun Dass <ad.sam007@gmail.com>
2024-12-08 22:35:33 -0800

    Lowest common ancestor

---
+++ Data Structures/src/questions/leetcode/LowestCommonAncestor3.java
@@ -0,0 +1,29 @@
+package questions.leetcode;
+
+public class LowestCommonAncestor3 {
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    };
+
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Node pointer1 = p;
+        Node pointer2 = q;
+        while(pointer1 != pointer2) {
+            if (pointer1.parent != null) {
+                pointer1 = pointer1.parent;
+            } else {
+                pointer1 = q;
+            }
+
+            if (pointer2.parent != null) {
+                pointer2 = pointer2.parent;
+            } else {
+                pointer2 = p;
+            }
+        }
+        return pointer1;
+    }
+}
