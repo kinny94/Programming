4286e0eb492759f57df7f53d0868f8b8a0dab517
Arjun Dass <ad.sam007@gmail.com>
2024-12-08 23:05:47 -0800

    Fast Slow pointer

---
+++ Data Structures/src/questions/leetcode/MiddleOfLinkedList.java
@@ -0,0 +1,33 @@
+package questions.leetcode;
+
+public class MiddleOfLinkedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
