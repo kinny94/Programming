8ae26b347665d7cd836255cd348da71f8b0fc442
Arjun Dass <ad.sam007@gmail.com>
2024-12-13 11:34:18 -0800

    remove duplicates in sorted list

---
+++ Data Structures/src/questions/leetcode/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,24 @@
+package questions.leetcode;
+
+public class RemoveDuplicatesFromSortedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while(current!=null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
