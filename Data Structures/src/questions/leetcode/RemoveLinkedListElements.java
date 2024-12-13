ccbc9c676ca9eff281f84a7d4528794ddf539fd6
Arjun Dass <ad.sam007@gmail.com>
2024-12-13 13:04:18 -0800

    remove linked list elements

---
+++ Data Structures/src/questions/leetcode/RemoveLinkedListElements.java
@@ -0,0 +1,30 @@
+package questions.leetcode;
+
+public class RemoveLinkedListElements {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = head;
+        ListNode prev = dummy;
+
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+                curr = curr.next;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+        return dummy.next;
+    }
+}
