5269e87e25352b01528f6504d4acf9f500cf3801
Arjun Dass <ad.sam007@gmail.com>
2024-12-13 02:03:37 -0800

    Reverse LinkedList II

---
+++ Data Structures/src/questions/leetcode/ReverseLinkedListII.java
@@ -0,0 +1,34 @@
+package questions.leetcode;
+
+public class ReverseLinkedListII {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        for (int i=0; i<left - 1; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+        for (int i=0; i<right - left; i++){
+            ListNode temp = curr.next;
+            curr.next = temp.next;
+            temp.next = prev.next;
+            prev.next = temp;
+        }
+        return dummy.next;
+    }
+}
