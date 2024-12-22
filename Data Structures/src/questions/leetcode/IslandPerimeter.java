0a0d492498854fa6cd27530146091606987812b8
Arjun Dass <ad.sam007@gmail.com>
2024-12-21 20:54:51 -0800

    Island Perimeter

---
+++ Data Structures/src/questions/leetcode/IslandPerimeter.java
@@ -0,0 +1,23 @@
+package questions.leetcode;
+
+public class IslandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        for (int i=0; i<grid.length; i++) {
+            for (int j=0; j<grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter = perimeter + 4;
+
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        perimeter = perimeter - 2;
+                    }
+
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        perimeter = perimeter - 2;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+}
