f409034d156ec6ed05f5fa8b5e607393e80ad2d0
Arjun Dass <ad.sam007@gmail.com>
2024-12-25 21:31:25 -0800

    Symmetrixc tree

---
+++ Data Structures/src/questions/leetcode/SymmetricTree.java
@@ -0,0 +1,50 @@
+package questions.leetcode;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class SymmetricTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if (left == null && right == null) {
+                continue;
+            }
+
+            if (left == null || right == null) {
+                return false;
+            }
+
+            if (left.val != right.val) {
+                return false;
+            }
+
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+
+        return true;
+    }
+}
