fa95863a500faf45f864a905dbf7217e0d4cbbb1
Arjun Dass <ad.sam007@gmail.com>
2024-12-25 16:50:07 -0800

    xag level order traversal\

---
+++ Data Structures/src/questions/leetcode/BinaryTreeZigZagLevelOrderTraversal.java
@@ -0,0 +1,67 @@
+package questions.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Deque;
+
+public class BinaryTreeZigZagLevelOrderTraversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> results = new ArrayList<>();
+
+        if (root == null) {
+            return results;
+        }
+
+        Deque<TreeNode> dq = new LinkedList<>();
+        dq.add(root);
+        boolean reverse = false;
+
+        while(!dq.isEmpty()) {
+            int size = dq.size();
+            results.add(new ArrayList<>());
+
+            for (int i=0; i<size; i++) {
+                if (!reverse) {
+                    TreeNode node = dq.pollFirst();
+                    results.get(results.size() - 1).add(node.val);
+
+                    if (node.left != null) {
+                        dq.addLast(node.left);
+                    }
+
+                    if (node.right != null) {
+                        dq.addLast(node.right);
+                    }
+                } else {
+                    TreeNode node = dq.pollLast();
+                    results.get(results.size() - 1).add(node.val);
+
+                    if (node.right != null) {
+                        dq.addFirst(node.right);
+                    }
+
+                    if (node.left != null) {
+                        dq.addFirst(node.left);
+                    }
+                }
+            }
+            reverse = !reverse;
+        }
+        return results;
+    }
+}
