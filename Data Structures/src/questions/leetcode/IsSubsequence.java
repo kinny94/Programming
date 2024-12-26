862e4f862c1a0c8e527739558fed152dd73dafa7
Arjun Dass <ad.sam007@gmail.com>
2024-12-26 01:28:17 -0800

    IsSubsequence

---
+++ Data Structures/src/questions/leetcode/IsSubsequence.java
@@ -0,0 +1,18 @@
+package questions.leetcode;
+
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+
+        int sp = 0;
+        int tp = 0;
+
+        while (sp < s.length() && tp < t.length()) {
+            if (s.charAt(sp) == t.charAt(tp)) {
+                sp++;
+            }
+            tp++;
+        }
+
+        return sp == s.length();
+    }
+}
