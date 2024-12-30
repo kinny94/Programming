8fad2fec4ccddfc7dabbdc7ef62b21e27951d10c
Arjun Dass <ad.sam007@gmail.com>
2024-12-30 15:47:00 -0800

    Insert Delete get random

---
+++ Data Structures/src/questions/leetcode/InsertDeleteRandomO1.java
@@ -0,0 +1,52 @@
+package questions.leetcode;
+
+import java.util.*;
+
+class InsertDeleteRandomO1 {
+
+    Map<Integer, Integer> map;
+    List<Integer> list;
+    Random rand;
+
+    public InsertDeleteRandomO1() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        rand = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+
+        int last = list.get(list.size() - 1);
+        int index = map.get(val);
+        list.set(index, last);
+        map.put(last, index);
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
