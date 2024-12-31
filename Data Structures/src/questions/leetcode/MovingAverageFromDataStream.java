ff243d1006e95edce4f19a800b9053bd998ac5f6
Arjun Dass <ad.sam007@gmail.com>
2024-12-30 21:57:22 -0800

    Moving average from data stream

---
+++ Data Structures/src/questions/leetcode/MovingAverageFromDataStream.java
@@ -0,0 +1,34 @@
+package questions.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MovingAverageFromDataStream {
+
+    private int size;
+    private Queue<Integer> queue;
+    private double windowSum;
+
+    public MovingAverageFromDataStream(int size) {
+        this.size = size;
+        this.queue = new LinkedList<>();
+        windowSum = 0;
+    }
+
+    public double next(int val) {
+        queue.add(val);
+        windowSum += val;
+
+        if (queue.size() > size) {
+            windowSum -= queue.poll();
+        }
+
+        return windowSum / queue.size();
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
