ba8fc1479ef4ad8299f1e67bad8e1b816a5d04cf
Arjun Dass <ad.sam007@gmail.com>
2024-12-31 21:51:06 -0800

    Sum Root to leaf nodes

---
+++ Data Structures/src/questions/leetcode/SumRootToLeafNumbers.java
@@ -0,0 +1,40 @@
+package questions.leetcode;
+
+public class SumRootToLeafNumbers {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int sumNumbers(TreeNode root) {
+        return solve(root, 0);
+    }
+
+    private int solve(TreeNode node, int pathSum) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        pathSum = pathSum * 10 + node.val;
+
+        if (node.left == null && node.right == null) {
+            return pathSum;
+        }
+
+        int left = solve(node.left , pathSum);
+        int right = solve(node.right, pathSum);
+
+        return  left + right;
+
+    }
+}
