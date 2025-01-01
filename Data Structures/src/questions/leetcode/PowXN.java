db5c7acd51df180114f0a8f021a7ce473cba264f
Arjun Dass <ad.sam007@gmail.com>
2024-12-31 23:33:32 -0800

    Pow x n

---
+++ Data Structures/src/questions/leetcode/PowXN.java
@@ -0,0 +1,23 @@
+package questions.leetcode;
+
+public class PowXN {
+    public double myPow(double x, int n) {
+        return binaryExp(x, (long) n);
+    }
+
+    private double binaryExp(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            return 1.0 / binaryExp(x, -n);
+        }
+
+        if (n % 2 == 1) {
+            return x * binaryExp(x * x, (n - 1) / 2);
+        } else {
+            return binaryExp(x * x, n / 2);
+        }
+    }
+}
