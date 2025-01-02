9d7ce7163d2660b9f14d657e683e6ee8799e44bc
Arjun Dass <ad.sam007@gmail.com>
2025-01-01 20:19:39 -0800

    add two numbers

--- Data Structures/src/questions/leetcode/NestListWeightsum.java
+++ Data Structures/src/questions/leetcode/NestListWeightsum.java
@@ -23,41 +23,38 @@ public class NestListWeightsum {
         public List<NestedInteger> getList();
     }
 
-    class NestListWeightsum {
-        public int depthSum(List<NestedInteger> nestedList) {
-            Queue<NestedInteger> queue = new LinkedList<>();
-            queue.addAll(nestedList);
+    public int depthSum(List<NestedInteger> nestedList) {
+        Queue<NestedInteger> queue = new LinkedList<>();
+        queue.addAll(nestedList);
 
-            int depth = 1;
-            int total = 0;
+        int depth = 1;
+        int total = 0;
 
-            while(!queue.isEmpty()) {
-                int size = queue.size();
-                for (int i=0; i<size; i++) {
-                    NestedInteger ni = queue.poll();
-                    if (ni.isInteger()) {
-                        total += ni.getInteger() * depth;
-                    } else {
-                        queue.addAll(ni.getList());
-                    }
-                }
-                depth++;
-            }
-            return total;
-            // return dfs(nestedList, 1);
-        }
-
-        public int dfs(List<NestedInteger> list,  int depth) {
-            int total = 0;
-            for (int i=0; i<list.size(); i++) {
-                if (list.get(i).isInteger()) {
-                    total += list.get(i).getInteger() * depth;
-                } else{
-                    total += dfs(list.get(i).getList(), depth + 1);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i=0; i<size; i++) {
+                NestedInteger ni = queue.poll();
+                if (ni.isInteger()) {
+                    total += ni.getInteger() * depth;
+                } else {
+                    queue.addAll(ni.getList());
                 }
             }
-            return total;
+            depth++;
         }
+        return total;
+        // return dfs(nestedList, 1);
+    }
 
+    public int dfs(List<NestedInteger> list,  int depth) {
+        int total = 0;
+        for (int i=0; i<list.size(); i++) {
+            if (list.get(i).isInteger()) {
+                total += list.get(i).getInteger() * depth;
+            } else{
+                total += dfs(list.get(i).getList(), depth + 1);
+            }
+        }
+        return total;
     }
 }
