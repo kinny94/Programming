7d99006aaa73f804e7d7f293204695fdec0cae09
Arjun Dass <ad.sam007@gmail.com>
2025-01-06 22:47:10 -0800

    basic calculator

--- Data Structures/src/questions/leetcode/BasicCalculatorII.java
+++ Data Structures/src/questions/leetcode/BasicCalculatorII.java
@@ -4,7 +4,11 @@ import java.util.Stack;
 
 public class BasicCalculatorII {
     public int calculate(String s) {
-        if (s == null || s.isEmpty()) {
+
+        /*
+
+            USing Stack
+             if (s == null || s.isEmpty()) {
             return 0;
         }
         int len = s.length();
@@ -35,6 +39,42 @@ public class BasicCalculatorII {
         while (!stack.isEmpty()) {
             result += stack.pop();
         }
+        return result;
+         */
+
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int currentNumber = 0;
+        int lastNumber = 0;
+        int result = 0;
+        char operation = '+';
+
+        for (int i=0; i<s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = currentNumber * 10 + Character.getNumericValue(currentChar);
+            }
+
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == s.length() - 1) {
+                if (operation == '+') {
+                    result += lastNumber;
+                    lastNumber = currentNumber;
+                } else if (operation == '-') {
+                    result += lastNumber;
+                    lastNumber = -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
+                }
+
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        result += lastNumber;
         return result;
     }
 }
