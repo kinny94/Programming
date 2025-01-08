4ac06f6f092b2d018fe4e19bd52058adfce8ef13
Arjun Dass <ad.sam007@gmail.com>
2025-01-07 19:06:02 -0800

    ClosestBinary Search Tree Value

---
+++ Data Structures/src/questions/leetcode/ClosestBinarySearchTreevalue.java
@@ -0,0 +1,35 @@
+package questions.leetcode;
+
+public class ClosestBinarySearchTreevalue {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int closestValue(TreeNode root, double target) {
+
+        TreeNode node = root;
+        int closest = root.val;
+        while(node != null) {
+            if (Math.abs(node.val - target) < Math.abs(closest - target)) {
+                closest = node.val;
+            }
+
+            if ((Math.abs(node.val - target) == Math.abs(closest - target)&& node.val < closest)) {
+                closest = node.val;
+            }
+
+            node = target < node.val ? node.left : node.right;
+        }
+        return closest;
+    }
+}
