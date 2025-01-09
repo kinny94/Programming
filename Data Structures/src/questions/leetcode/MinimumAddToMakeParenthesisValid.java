299031dc07355569e4b3d9ef41fab7103debcc42
Arjun Dass <ad.sam007@gmail.com>
2025-01-08 18:30:44 -0800

    Minimum ad to make parenthesis valid

---
+++ Data Structures/src/questions/leetcode/MinimumAddToMakeParenthesisValid.java
@@ -0,0 +1,23 @@
+package questions.leetcode;
+
+public class MinimumAddToMakeParenthesisValid {
+    public int minAddToMakeValid(String s) {
+        int openBrackets = 0;
+        int minAddsRequired = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                openBrackets++;
+            } else {
+
+                if (openBrackets > 0) {
+                    openBrackets--;
+                } else {
+                    minAddsRequired++;
+                }
+            }
+        }
+
+        return minAddsRequired + openBrackets;
+    }
+}
