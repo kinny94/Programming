f913a447a8d365cc03d5a82693a710a31aae1313
Arjun Dass <ad.sam007@gmail.com>
2025-01-09 20:21:42 -0800

    Longest common prefix

--- Data Structures/src/questions/leetcode/LongestCommonPrefix.java
+++ Data Structures/src/questions/leetcode/LongestCommonPrefix.java
@@ -1,15 +1,42 @@
 package questions.leetcode;
 
+import java.util.Arrays;
+
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++) while (
-                strs[i].indexOf(prefix) != 0
-        ) {
-            prefix = prefix.substring(0, prefix.length() - 1);
-            if (prefix.isEmpty()) return "";
+        if (strs == null || strs.length == 0) {
+            return "";
         }
-        return prefix;
+
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length - 1];
+
+        int minLength = Math.min(s1.length(), s2.length());
+        int index = 0;
+        for (int i=0; i<minLength; i++) {
+            if (s1.charAt(i) == s2.charAt(i)) {
+                index++;
+            } else {
+                break;
+            }
+        }
+
+        return s1.substring(0, index);
+    }
+
+    public String verticalScan(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c
+                ) return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
     }
 }
