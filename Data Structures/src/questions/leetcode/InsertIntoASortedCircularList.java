18d8ba67398640b6383cd6109006ad78dc0c6995
Arjun Dass <ad.sam007@gmail.com>
2025-01-10 13:03:22 -0800

    Insert into a circular sorted list

---
+++ Data Structures/src/questions/leetcode/InsertIntoASortedCircularList.java
@@ -0,0 +1,32 @@
+package questions.leetcode;
+
+public class InsertIntoASortedCircularList {
+    public Node insert(Node head, int insertVal) {
+        Node newNode = new Node(insertVal);
+        if (head == null) {
+            newNode.next = newNode;
+            return newNode;
+        }
+
+
+        Node node = head.next;
+        Node prev = head;
+        while (node != head) {
+
+            if (prev.val <= insertVal && insertVal <= node.val) {
+                break;
+            }
+            if (prev.val > node.val && (insertVal >= prev.val || insertVal <= node.val)) {
+                break;
+            }
+
+            prev = node;
+            node = node.next;
+        }
+
+        prev.next = newNode;
+        newNode.next = node;
+        return head;
+    }
+
+}
