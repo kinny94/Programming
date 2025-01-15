7326e0b5374215716e62d43e057cc59f3e15c073
Arjun Dass <ad.sam007@gmail.com>
2025-01-15 13:42:59 -0800

    contains Duplicate

---
+++ Data Structures/src/questions/leetcode/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package questions.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicate {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> seen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            if (seen.containsKey(val) && i - seen.get(val) <= k) {
+                return true;
+            }
+            seen.put(val, i);
+        }
+
+        return false;
+    }
+}
