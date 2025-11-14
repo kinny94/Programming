b7be52bc2b1349c04f3fc27a889f992afa077f13
Arjun Dass <ad.sam007@gmail.com>
2025-11-14 11:39:39 -0800

    Bridge pattern

---
+++ Design Pattern/Gamma Categorization/Bridge/Bridge.java
@@ -0,0 +1,81 @@
+// This pattern is all about connecting components together through abstractions.
+// It prevents a 'cartesian product' complexity explosion.
+// It prevents "entity explosion" by representing things as a sum of primitive operations.
+// A mechanism that decouples an interface (hierarchy) from an implementation (hierarchy).
+
+interface Renderer {
+    void renderCircle(float radius);
+}
+
+class VectorRenderer implements Renderer {
+    @Override
+    public void renderCircle(float radius) {
+        System.out.println("Drawing a circle of radius " + radius);
+    }
+}
+
+class RasterRenderer implements Renderer {
+    @Override
+    public void renderCircle(float radius) {
+        System.out.println("Drawing pixels for a circle of radius " + radius);
+    }
+}
+
+abstract class Shape {
+    protected Renderer renderer;
+
+    public Shape(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public abstract void draw();
+    public abstract void resize(float factor);
+}
+
+class Circle extends Shape {
+    public float radius;
+
+
+    @Inject
+    public Circle(Renderer renderer) {
+        super(renderer);
+    }
+
+    public Circle(Renderer renderer, float radius) {
+        super(renderer);
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        renderer.renderCircle(radius);
+    }
+
+    @Override
+    public void resize(float factor) {
+        radius *= factor;
+    }
+}
+
+class ShapeModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(Renderer.class).to(VectorRenderer.class);
+    }
+}
+
+public class Bridge {
+    public static void main(String[] args) {
+    //    RasterRenderer rasterRenderer = new RasterRenderer();
+    //    VectorRenderer vectorRenderer = new VectorRenderer();
+    //    Circle circle = new Circle(vectorRenderer, 5);
+    //    circle.draw();
+    //    circle.resize(2);
+    //    circle.draw();
+        Injector injector = Guice.createInjector(new ShapeModule());
+        Circle circle = injector.getInstance(Circle.class);
+        circle.draw();
+        circle.resize(2);
+        circle.draw();
+    }
+}
